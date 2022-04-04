033536d3effa40292d6711fe444f7c1ca9a41aa7
Donald D'aquin Komguem Fotso <donalddev99@gmail.com>
2022-04-04 12:03:59 +0200

    Update infra folders

--- infra/env/prod/start.sh
+++ infra/env/prod/start.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 <<<<<<< HEAD
+<<<<<<< HEAD
 cd "app/frontend/prod" || exit
 docker compose up --build -d  
 =======
 ls
 >>>>>>> 34a778e (Set up infra as code)
+=======
+cd "app/frontend/prod" || exit
+docker compose down
+docker rmi registry.dsp-archiwebo21-ct-df-an-cd.fr/stable/frontend
+docker compose up --build -d  
+>>>>>>> e955586 (Update infra folders)
