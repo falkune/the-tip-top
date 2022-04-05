5fb08fbe3147711839f4e639577f51b9948095eb
Donald D'aquin Komguem Fotso <donalddev99@gmail.com>
2022-04-05 08:34:35 +0200

    Add COMPOSE_PROJECT ENV in release, remove YUP

--- infra/env/release/start.sh
+++ infra/env/release/start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 cd "app/frontend/release" || exit
-docker compose down
-docker rmi registry.dsp-archiwebo21-ct-df-an-cd.fr/release/frontend
+docker compose down --rmi all
 docker compose up --build -d  
