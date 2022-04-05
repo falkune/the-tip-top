f3f9315ba56921b4d30947d9e99add6b59440185
Donald D'aquin Komguem Fotso <donalddev99@gmail.com>
2022-04-05 09:27:44 +0200

    Update deploy script

--- infra/scripts/deploy.sh
+++ infra/scripts/deploy.sh
@@ -19,9 +19,9 @@ ssh -o StrictHostKeyChecking=no -l donald 45.155.170.65 "docker compose -f ./app
 =======
 
 if [ "$envr" = "release" ]; then
-  ssh -o StrictHostKeyChecking=no -l donald 45.155.170.65 "docker compose -f ./app/frontend/release/compose.yml down --rmi all && rm -r .app/frontend/release"
+  ssh -o StrictHostKeyChecking=no -l donald 45.155.170.65 "docker compose -f ./app/frontend/release/compose.yml down --rmi all && rm -r ./app/frontend/release"
 else
-  ssh -o StrictHostKeyChecking=no -l donald 45.155.170.65 "docker compose -f ./app/frontend/prod/compose.yml down --rmi all && rm -r .app/frontend/prod"
+  ssh -o StrictHostKeyChecking=no -l donald 45.155.170.65 "docker compose -f ./app/frontend/prod/compose.yml down --rmi all && rm -r ./app/frontend/prod"
 fi
 >>>>>>> 0f90135 (Update deploy script)
 
