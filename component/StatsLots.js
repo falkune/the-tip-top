e7e3453f048637d0c7b69a491b74e4e7cd95b0c1
diokel <iboudiokel@gmail.com>
2022-10-29 00:50:48 +0200

    update

--- component/StatsLots.js
+++ component/StatsLots.js
@@ -1,76 +1,64 @@
-import React, { useEffect, useRef, useState } from "react";
-import Chart from "chart.js/auto";
-import axios from "axios";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../image/logo.png";
+import OneLot from "./OneLot";
 
-export default function StatInscription({ days,  idSession}) {
-  const canvasEl = useRef(null);
-
-  useEffect(() => {
-    // console.log(days, idSession)
-    getRegistrationByDay();
-  },[days]);
-
-  // this function get the number of registration by day
-  const getRegistrationByDay = async () => {
-    const accessToken = localStorage.getItem('token');
-    const url = "https://api.dev.dsp-archiwebo21-ct-df-an-cd.fr/user/registration-by-day"
-    const response = await axios.get(url, { headers: { "Authorization" : `Bearer ${accessToken}` }})
-    const registrationByDay = Array();
-    response.data.forEach(e => {
-      registrationByDay.push(e.nomberOfRegitration)
-    })
-    drawChart(registrationByDay)
-
-    // const body = {
-    //  sessionId : idSession
-    // };
-
-    // try {
-    //   let createdSession = await axios.post(url, body, config);
-    //   console.log(createdSession);
-    // } catch (e) {
-    //   console.log(e);
-    // }
-  }
-
-  // this function draw the chart of number of registred person by day
-  const drawChart = (registrationByDay) => {
-    const labels = Array();
-    for(let i = 1; i <= days; i++){
-      labels.push("Jour "+i)
-    }
-    const ctx = canvasEl.current.getContext("2d");
-    const data = {
-      labels: labels,
-      datasets: [
-        {
-          label: "Nombre d'inscrit par jour",
-          data: registrationByDay,
-          fill: true,
-          borderWidth: 2,
-          borderColor: "#41C2B0",
-          lineTension: 0.3,
-          pointRadius: 1.5,
-        },
-      ],
-    };
-
-    const config = {
-      responsive: true,
-      type: "line",
-      data: data,
-    };
-
-    const myLineChart = new Chart(ctx, config);
-
-    return function cleanup() {
-      myLineChart.destroy();
-    };
-  }
-
+const StatsLots = ({ data }) => {
+  
+  const lots = [
+    {
+      now:200,
+      max:1000,
+      title:" Infuseur à thé",
+      color:"#41C2B0",
+      value: 19
+    },
+    {
+      now:3600,
+      max:5000,
+      title:"Boite de 100g d’un thé détox ",
+      value: 21
+    }, 
+    {
+      now:4500,
+      max:10000,
+      title:"Boite de 100g d’un thé signature",
+      color:"#41C2B0",
+      value: 32
+    },
+    {
+      now:5000,
+      max:1000000,
+      title:'Coffret découverte',
+      value: 56
+    } 
+  ] 
+   
   return (
-    <div style={{ backgroundColor: "white", padding: 25, borderRadius: 10 }}>
-      <canvas ref={canvasEl} height={80} width={500} />
-    </div>
-  );
+  <div style={styles.lot} >
+    { 
+      lots.map((l,index)=> (
+        <OneLot 
+          key={index}
+          title={l.title}
+          now={l.now}
+          max={l.max}
+          image={l.image}
+          value={l.value}/> ))
+    }
+  </div>
+  )
 }
+export default StatsLots;
+
+const styles = {
+  lot: {
+    display: "flex",
+    padding: 15,
+    width: "100%",
+    marginBottom: 25,
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+  },
+};
