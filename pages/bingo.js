fd7202805496ad44271079282c6f866c408a2dfd
diokel <iboudiokel@gmail.com>
2022-11-16 13:24:21 +0100

    fix ag-grid-community module not found

--- pages/bingo.js
+++ pages/bingo.js
@@ -1,28 +1,29 @@
 import React, { useEffect, useState } from "react";
-import Head from 'next/head'
-import Link from "next/link"
-import styles from '../styles/Home.module.css'
-import Header from '../component/Header'
-import Footer from "../component/Footer"
-import { useRouter } from 'next/router'
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import Header from "../component/Header";
+import Footer from "../component/Footer";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 export default function Bingo() {
-  const router = useRouter();
-  const [num, setNum] = useState(
-    router.query.number ? router?.query?.number : null
-  );
+  const [num, setNum] = useState(null);
   const [current, setCurrent] = useState("");
 
   useEffect(() => {
     getCurrent();
   }, []);
 
+  const router = useRouter();
   console.log(num, "num");
   const goResult = () => {
-    router.push({
-      pathname: `resultat`,
-      query: { number: num },
-    });
+    if (num.length === 10) {
+      router.push({
+        pathname: `resultat`,
+        query: { number: num },
+      });
+    } else console.log("perdu");
   };
   const getCurrent = async () => {
     //fonction pour créer un ticket
@@ -46,11 +47,6 @@ export default function Bingo() {
     }
   };
 
-  const handleNameChange = (e) => {
-    const limit = 10;
-    setNum(e.target.value.slice(0, limit));
-  };
-
   return (
     <div className={styles.container}>
       <Head>
@@ -61,30 +57,50 @@ export default function Bingo() {
       <Header />
       <section className={styles.block}>
         <h1 className={styles.h1}>Bingo ticket</h1>
-
-        <p
-          style={{
-            fontWeight: 600,
-            fontSize: "1.2em",
-            padding: 15,
-            marginBottom: 5,
-            color: "#ACACAC",
-          }}
-        >
-          Tester votre ticket pour voir votre lot remporté (100% gagnant )
-        </p>
+        <p>Tester votre ticket pour voir votre lot remporté (100% gagnant )</p>
+        <Link href="/#lots">
+          <small style={{ color: "#41D8C2" }}>Voir les differents lots</small>
+        </Link>
         <form className={styles.bingo}>
           <input
             type="number"
             name="numero"
-            maxLength={2}
-            min={0}
-            max={99}
             autoComplete="off"
             placeholder="Veuillez rentrer vos 10 numéros"
-            onChange={handleNameChange}
+            maxLength={10}
+            onChange={(e) => {
+              setNum(e.target.value);
+            }}
             value={num}
           />
+          {num !== null && num.length < 10 && (
+            <small style={{ color: "red", textDecoration: "none" }}>
+              Numéro invalide
+            </small>
+          )}
+          {num !== null && num.length === 10 && (
+            <small style={{ color: "green", textDecoration: "none" }}>
+              Numéro valide
+            </small>
+          )}
+          {num !== null && num.length === 10 ? (
+            <button
+              type="button"
+              onClick={goResult}
+              className={styles.action}
+              style={{ margin: 25 }}
+            >
+              Valider
+            </button>
+          ) : (
+            <button
+              disabled={true}
+              className={styles.noaction}
+              style={{ margin: 25 }}
+            >
+              Valider
+            </button>
+          )}
         </form>
         {num !== null && num.length < 10 && (
           <small
