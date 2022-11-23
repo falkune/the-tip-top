d97ba4c55f8c8d98f7a0d04cf43e9bea7a5f5696
Cheikh THIAM <cheikhthiam95g@gmail.com>
2022-11-23 01:40:12 +0100

    Fix typo : TeaBingo.png instead of teaBingo.png

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState ,useCallback } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import teab from "../image/teaBingo.png"
+import teab from "../image/TeaBingo.png"
 import Header from "../component/Header";
 import Footer from "../component/Footer";
 import { useRouter } from "next/router";
