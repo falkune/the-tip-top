0b471ea065948a189df25305e087a10c0cf2c35d
Cheikh THIAM <cthiam@fluxod.com>
2022-11-29 09:34:23 +0100

    changing react version for test jest

---
+++ tests/pages/index.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "../test-utils";
+import Footer from "../../component/Footer";
+
+describe("Footer", () => {
+  it("should render the heading", () => {
+    const textToFind = "S'inscrire à la newsletter"
+
+    render(<Footer />,{ legacyRoot: true } );
+    const heading = screen.getByText(textToFind);
+
+    expect(heading).toBeInTheDocument();
+  });
+});
