ca453c9a23d0684342d46e591bd961a629781fbb
diokel <iboudiokel@gmail.com>
2022-12-04 23:18:13 +0100

    add firebase for facebook and google registration

---
+++ config/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyA0g7hFyn7-UMWYY8kexVE6W-_TbJz4d3Q",
+  
+    authDomain: "thetiptop-370410.firebaseapp.com",
+  
+    projectId: "thetiptop-370410",
+  
+    storageBucket: "thetiptop-370410.appspot.com",
+  
+    messagingSenderId: "962889083378",
+  
+    appId: "1:962889083378:web:729f99d2a07d9a2316636b"
+  
+};
+  
+
+export const app = initializeApp(firebaseConfig);
