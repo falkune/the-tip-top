d32b12876c312dbbd73cb59000e89ab8f525614e
Donald D'aquin Komguem Fotso <donalddev99@gmail.com>
2022-12-05 15:26:27 +0100

    Update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://dev.dsp-archiwebo21-ct-df-an-cd.fr/',
+    siteUrl: process.env.SITE_URL,
     generateRobotsTxt: true, // (optional)
     // ...other options
   }
