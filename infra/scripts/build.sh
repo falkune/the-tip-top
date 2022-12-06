76f92b6dadef0ac0167f2e22ee9754271184e674
Donald D'aquin Komguem Fotso <donalddev99@gmail.com>
2022-12-06 14:55:24 +0100

    Update docker file of frontend for add staging and production target

--- infra/scripts/build.sh
+++ infra/scripts/build.sh
@@ -10,9 +10,14 @@ type=$2
 tag=${IMAGE_TAG}
 
 formated_image=${DOCKER_PRIVATE_REGISTER}/${type}/${image}:${tag}
-
 formated_image_latest=${DOCKER_PRIVATE_REGISTER}/${type}/${image}:"latest"
 
-docker build --no-cache -t "${formated_image}" -f infra/build/"${image}"/Dockerfile .
+if [ "$type" = "release" ]; then
+   docker build --target staging --no-cache -t "${formated_image}" -f infra/build/"${image}"/Dockerfile .  
+else
+  docker build --target production --no-cache -t "${formated_image}" -f infra/build/"${image}"/Dockerfile .  
+fi
 
 docker tag "${formated_image}" "${formated_image_latest}"
+
+
