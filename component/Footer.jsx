5e70310c0a13ef3e446a5d0ac522449798c057ff
diokel <iboudiokel@gmail.com>
2022-12-11 20:16:33 +0100

    fixe SyntaxError: /var/jenkins_home/workspace/TheTipTop-Frontend_dev/component/Footer.jsx: Support for the experimental syntax 'jsx' isn't currently enabled (31:5):

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -14,115 +14,114 @@ const style = {
     bgcolor: 'background.paper',
     boxShadow: 24,
     p: 4,
-    display:"flex",
-    flexDirection:"column",
-    alignItems:"center",
-    justifyContent:"center",
-    background:"#38870D",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    background: "#38870D",
 
-  };
-  
+};
+
 
 const Footer = () => {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-  return (
-    <footer style={styles.footer}>  
-        <button  onClick={handleOpen}>
-            S'inscrire à la newsletter
-       </button>
-        <nav style={styles.nav} >
-            <small>Copyright © 2022 - TipTop </small>
-             <ul style={styles.ul} >
-             <Link href="/politique"> 
-                 <li style={styles.li}>Politique de confidentialié </li>
-             </Link>
-             <Link href="/condition"> 
-                 <li style={styles.li}>Mention légales</li>
-             </Link>
-             </ul>
-         </nav>
-         <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description">
+    return (
+        <footer style={styles.footer}>
+            <button onClick={handleOpen}>
+                S'inscrire à la newsletter
+            </button>
+            <nav style={styles.nav}>
+                <small>Copyright © 2022 - TipTop </small>
+                <ul style={styles.ul} >
+                    <Link href="/politique">
+                        <li style={styles.li}>Politique de confidentialié </li>
+                    </Link>
+                    <Link href="/condition">
+                        <li style={styles.li}>Mention légales</li>
+                    </Link>
+                </ul>
+            </nav>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description">
                 <div style={style}>
-                     <form style={styles.form}>
+                    <form style={styles.form}>
                         <h3>Newsletter</h3>
                         <small>Suivre notre actualité</small>
-                        <input style="input" placeholder="Nom" type="text"/>
-                        <input style="input" placeholder="Email" type="email"/>
+                        <input style="input" placeholder="Nom" type="text" />
+                        <input style="input" placeholder="Email" type="email" />
                         <button style={styles.button}>S'inscrire</button>
-                     </form>
+                    </form>
                 </div>
-
-         </Modal>
+            </Modal>
 
         </footer>
-  )
+    )
 }
 
 export default Footer
 
 const styles = {
 
-    nav:{
-        marginTop:10,
-        marginBottom:20,
-        display:"flex",
-        flexWrap:"wrap-reverse",
-        width:"100%",
-        justifyContent:"center"
+    nav: {
+        marginTop: 10,
+        marginBottom: 20,
+        display: "flex",
+        flexWrap: "wrap-reverse",
+        width: "100%",
+        justifyContent: "center"
     },
 
-    form:{
-        display:"flex",
-        flexDirection:"column",
-        justifyContent:"center",
-        alignItems:"center",
-        padding:10,
-        textAlign:"center",
-        color:"white",
-        width:"100%"
+    form: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 10,
+        textAlign: "center",
+        color: "white",
+        width: "100%"
     },
 
-    input:{
-        height:50,
-        marginTop:10,
-        marginBottom:5,
-        border:"none",
-        padding:8,
-        paddingLeft:15,
-        background:"#F9F9F9",
-        width:"90%",
-        fontWeight:"bold"
+    input: {
+        height: 50,
+        marginTop: 10,
+        marginBottom: 5,
+        border: "none",
+        padding: 8,
+        paddingLeft: 15,
+        background: "#F9F9F9",
+        width: "90%",
+        fontWeight: "bold"
 
     },
 
-    button:{
-        height:40,
-        border:"none",
-        margin:10,
-        width:"95%",
-        fontSize:16,
-        color:"white",
-        background:"#2D6005",
-        height:50,
-        borderRadius:8
+    button: {
+        height: 40,
+        border: "none",
+        margin: 10,
+        width: "95%",
+        fontSize: 16,
+        color: "white",
+        background: "#2D6005",
+        height: 50,
+        borderRadius: 8
     },
-  
-    ul:{
-        display:"flex",
-        flexWrap:"wrap",
+
+    ul: {
+        display: "flex",
+        flexWrap: "wrap",
     },
 
 
-    li:{
-       listStyleType:"none",
-       marginLeft:10,
-       textAlign:"center"
+    li: {
+        listStyleType: "none",
+        marginLeft: 10,
+        textAlign: "center"
     },
 
 
