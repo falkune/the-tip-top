5d79dd4790bc17321fc312f57ed6d554ec3035cc
diokel <iboudiokel@gmail.com>
2022-12-11 14:07:29 +0100

    update pieChart style

--- component/AllStats.js
+++ component/AllStats.js
@@ -5,7 +5,7 @@ import ParticipationStat from './ParticipationStat';
 import ApiContext from '../context/apiContext';
 import { getDaysBetweenTwoDates } from "../fonctions/utils";
 import { getSessionDetails } from '../fonctions/sessions';
-import {statLots} from '../fonctions/tickets';
+import { statLots } from '../fonctions/tickets';
 import AgeStat from './AgeStat';
 
 const AllStats = (props) => {
@@ -20,41 +20,41 @@ const AllStats = (props) => {
     setAsignTicket(0)
     getAsignTicket(context, props.idSession);
     getDetailsSession(context, props.idSession);
-  },[props.idSession])
+  }, [props.idSession])
 
   const getDetailsSession = (context, idSession) => {
     getSessionDetails(context, idSession)
-    .then((response) => {
-      setLimitTicket(response.limitTicket)
-      setNumberDay(getDaysBetweenTwoDates(new Date(response.endDate), new Date(response.startDate)))
-    })
+      .then((response) => {
+        setLimitTicket(response.limitTicket)
+        setNumberDay(getDaysBetweenTwoDates(new Date(response.endDate), new Date(response.startDate)))
+      })
   }
 
   const getAsignTicket = (context, idSession) => {
     statLots(context, idSession)
-    .then((response) => { 
-      if(!response.statusCode){
-        setAsignTicket(response.sessionStats.sessionTotalNumberOfTickets)
-        setPercentage(response.sessionStats.sessionTotalNumberOfTicketsPercentage);
-        setPercentageGenerate(response.sessionStats.sessionClaimbedTicketPercentage)
-      }
-    })
+      .then((response) => {
+        if (!response.statusCode) {
+          setAsignTicket(response.sessionStats.sessionTotalNumberOfTickets)
+          setPercentage(response.sessionStats.sessionTotalNumberOfTicketsPercentage);
+          setPercentageGenerate(response.sessionStats.sessionClaimbedTicketPercentage)
+        }
+      })
   }
 
   return (
     <div style={styles.stat}>
-      <ParticipationStat asignTicket={asignTicket} limitTicket={limitTicket} percentage={percentage} percentageGenerate={percentageGenerate} idSession={props.idSession}/>
-      <StatInscription days={numberDay} idSession={props.idSession}/>
-      <StatsLots idSession={props.idSession}/>
-      <AgeStat/>
+      <ParticipationStat asignTicket={asignTicket} limitTicket={limitTicket} percentage={percentage} percentageGenerate={percentageGenerate} idSession={props.idSession} />
+      <StatInscription days={numberDay} idSession={props.idSession} />
+      <StatsLots idSession={props.idSession} />
+      <AgeStat />
     </div>
   )
 }
 export default AllStats;
 
 const styles = {
-  stat:{
-    background:"none",
-    padding:25
+  stat: {
+    background: "none",
+    padding: 25
   }
 }
--- component/ParticipationStat.js
+++ component/ParticipationStat.js
@@ -1,31 +1,31 @@
-import React, {useEffect, useState, useContext} from "react";
+import React, { useEffect, useState, useContext } from "react";
 import ApiContext from '../context/apiContext';
-import {notifier, refreshToken} from '../fonctions/utils';
-import {claimedTickeBySession} from '../fonctions/tickets';
-import {CardSummary} from './CardSummary';
+import { notifier, refreshToken } from '../fonctions/utils';
+import { claimedTickeBySession } from '../fonctions/tickets';
+import { CardSummary } from './CardSummary';
 import PieGraph from './Piecharte';
 
 
-export default function ParticipationStat({asignTicket, limitTicket, percentage, percentageGenerate, idSession}) {
+export default function ParticipationStat({ asignTicket, limitTicket, percentage, percentageGenerate, idSession }) {
   const [numberOfClaimbedTicket, setNumberOfClaimbedTicket] = useState(0)
   const context = useContext(ApiContext);
 
   useEffect(() => {
-    if(idSession != ""){
+    if (idSession != "") {
       validationTicketsStats();
     }
   }, [idSession]);
 
   const validationTicketsStats = () => {
     claimedTickeBySession(context, idSession)
-    .then((response) => {
-      if(response.statusCode){
-        refreshToken(response, context);
-      }else{
-        setNumberOfClaimbedTicket(response.length);
-      }
-    })
-    .catch(() => notifier())
+      .then((response) => {
+        if (response.statusCode) {
+          refreshToken(response, context);
+        } else {
+          setNumberOfClaimbedTicket(response.length);
+        }
+      })
+      .catch(() => notifier())
   }
 
   return (
@@ -36,13 +36,13 @@ export default function ParticipationStat({asignTicket, limitTicket, percentage,
         asignTicket={asignTicket}
         claimbedTicket={numberOfClaimbedTicket}
         percentage={percentage}
-        percentageGenerate={percentageGenerate}/>
+        percentageGenerate={percentageGenerate} />
     </div>
   );
 }
 
 const styles = {
-  container:{
+  container: {
     display: 'flex',
     justifyContent: "space-around",
     flexWrap: "wrap",
