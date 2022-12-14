6a71913b087c27302df5b055f0d639c0342f0c20
diokel <iboudiokel@gmail.com>
2022-12-14 17:20:25 +0100

    fix style of cookies management component

--- component/cookiesManagement.jsx
+++ component/cookiesManagement.jsx
@@ -26,8 +26,8 @@ function CookiesManagement() {
                     Ce site utilise des cookies <a href="/condition" style={styles.link}>  en savoir plus</a>
                 </div>
                 <div>
-                    <button onClick={cookiesAccept} style={styles.btn}><FontAwesomeIcon style={styles.check} icon={faCheck} /> accepter</button>
-                    <button onClick={cookiesReject} style={styles.btn}><FontAwesomeIcon style={styles.mark} icon={faXmark} /> rejeter</button>
+                    <span onClick={cookiesAccept} style={styles.btn}><FontAwesomeIcon style={styles.check} icon={faCheck} /> accepter</span>
+                    <span onClick={cookiesReject} style={styles.btn}><FontAwesomeIcon style={styles.mark} icon={faXmark} /> rejeter</span>
                 </div>
             </div>
         )
