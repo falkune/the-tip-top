3071809b4b7d14565308d58be098f7f09b11339c
cherif diallo <diallcherif@gmail.com>
2022-12-14 20:53:59 +0100

    update title et meta description

--- component/DeliveredInfos.jsx
+++ component/DeliveredInfos.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Image from 'next/image';
 import check from "../image/check.png"
 
-export default function DeliveredInfos(props){
+export default function DeliveredInfos({value}){
+    console.log(value.row.isDelivered,"yay")
     return (
         <div style={{display:"flex",alignItems:"center"}}>
-            {  props.data.isDelivered ?
+            { value.row.isDelivered ?
             <Image height={20} width={20} src={check} alt=""/>
                  :
                  <p style={{color:"red",fontWeight:"bold",textAlign:"center",margin:0}}>Lot non délivré</p>
